48831ae46251b048323efad965a2fee99b1dca28
Adam <zealan@gmail.com>
2017-04-01 00:31:00 -0400

    create bathroom view

--- bathrooms/views.py
+++ bathrooms/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.conf.urls import url
+from django.http import HttpResponse
 
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+def index(request):
+    return HttpResponse("AIRBNB LIVES.")
