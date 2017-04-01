6059c60556ab76b8f13cdfdfaf70590fec41c33b
Adam <zealan@gmail.com>
2017-04-01 01:13:26 -0400

    add a bunch more fields

--- bathrooms/models.py
+++ bathrooms/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Bathroom(models.Model):
 	name = models.CharField(max_length=100)
 	urinals = models.PositiveSmallIntegerField()
+	stalls = models.PositiveSmallIntegerField()
+	handicap_stalls = models.PositiveSmallIntegerField()
+	sinks = models.PositiveSmallIntegerField()
+	paper_towels = models.PositiveSmallIntegerField()
+	hand_dryers = models.PositiveSmallIntegerField()
+	windows = models.PositiveSmallIntegerField()
 	
 	def __str__(self):
 		return self.name
