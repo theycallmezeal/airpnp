cbaa4c58f6cf7de3a43a906e079fca67e8c7a44e
Adam <zealan@gmail.com>
2017-04-01 01:03:27 -0400

    create views and models

--- bathrooms/models.py
+++ bathrooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Bathroom(models.Model):
+	name = models.CharField(max_length=100)
+	urinals = models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return self.name
--- bathrooms/views.py
+++ bathrooms/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Bathroom
 
 def index(request):
-    return HttpResponse("AIRBNB LIVES.")
+	bathroom_list = Bathroom.objects.all()
+	vars = {
+		'bathroom_list': bathroom_list
+	}
+	return render(request, 'index.html', vars)
+	
+def detail(request, bathroom_id):
+	bathroom = get_object_or_404(Bathroom, pk=bathroom_id)
+	vars = {
+		'bathroom': bathroom
+	}
+	return render(request, 'bathroom.html', vars)
